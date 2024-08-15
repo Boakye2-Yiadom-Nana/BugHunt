92a23609a2ec3f8a5640f37ebaba1ac20974fccd
Boakye2-Yiadom-Nana <136847960+Boakye2-Yiadom-Nana@users.noreply.github.com>
2024-08-15 20:58:46 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import 'bootstrap-icons/font/bootstrap-icons.css';
-import { AiFillHome } from "react-icons/ai";
+// import { AiFillHome } from "react-icons/ai";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import DashboardPage from './components/DashboardPage';
+// import DashboardPage from './components/DashboardPage';
 import UserPage from './components/UserPage';
 import Login from './components/Login';
 import Register from './components/Register';
